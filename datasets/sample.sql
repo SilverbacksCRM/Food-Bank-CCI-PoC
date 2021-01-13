48196690a4db321530d105300df60350b61a90a7
Orecchio, Tony <Anthony.Orecchio@salliemae.com>
2021-01-13 08:27:56 -0500

    backout delivery enhancements

--- datasets/sample.sql
+++ datasets/sample.sql
@@ -11,14 +11,13 @@ CREATE TABLE "Delivery_Item__c" (
 	id INTEGER NOT NULL, 
 	"Food_Expiration_Date__c" VARCHAR(255), 
 	"Food_Storage__c" VARCHAR(255), 
-	"Name" VARCHAR(255), 
 	"Delivery__c" VARCHAR(255), 
 	PRIMARY KEY (id)
 );
-INSERT INTO "Delivery_Item__c" VALUES(1,'2021-01-22','Frozen','Frozen Peas','2');
-INSERT INTO "Delivery_Item__c" VALUES(2,'2021-01-29','Non-refrigerated','Black Beans','2');
-INSERT INTO "Delivery_Item__c" VALUES(3,'2021-01-25','Non-refrigerated','Bow Tie Pasta','1');
-INSERT INTO "Delivery_Item__c" VALUES(4,'2021-02-05','Refrigerated','Fat-Free Milk','1');
+INSERT INTO "Delivery_Item__c" VALUES(1,'2021-01-22','Frozen','1');
+INSERT INTO "Delivery_Item__c" VALUES(2,'2021-01-25','Non-refrigerated','2');
+INSERT INTO "Delivery_Item__c" VALUES(3,'2021-01-29','Non-refrigerated','1');
+INSERT INTO "Delivery_Item__c" VALUES(4,'2021-02-05','Refrigerated','2');
 CREATE TABLE "Delivery__c" (
 	id INTEGER NOT NULL, 
 	"Scheduled_Date__c" VARCHAR(255), 
@@ -26,6 +25,6 @@ CREATE TABLE "Delivery__c" (
 	"Account__c" VARCHAR(255), 
 	PRIMARY KEY (id)
 );
-INSERT INTO "Delivery__c" VALUES(1,'2021-01-11T20:00:00.000+0000','Scheduled','2');
-INSERT INTO "Delivery__c" VALUES(2,'2021-01-13T20:00:00.000+0000','Scheduled','1');
+INSERT INTO "Delivery__c" VALUES(1,'2021-01-13T20:00:00.000+0000','Scheduled','1');
+INSERT INTO "Delivery__c" VALUES(2,'2021-01-11T20:00:00.000+0000','Scheduled','2');
 COMMIT;
